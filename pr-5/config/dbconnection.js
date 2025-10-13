870c88ff4cc8ad81cbf946b3e89decf1ca7916f2
Pruthviraj Solanki <pruthvirajsolanki125@gmail.com>
2025-10-13 16:43:22 +0530

    dbconnection.js

--- pr-5/config/dbconnection.js
+++ pr-5/config/dbconnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbconnection = () => {
-    mongoose.connect("mongodb+srv://fenamavani:fena1403@cluster0.v2tedkq.mongodb.net/pr-5-movies")
+    mongoose.connect("mongodb+srv://")
         .then(() => console.log("DB is connected..."))
         .catch(err => console.error("DB Connection Error:", err));
 };
