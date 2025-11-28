d22e8ef958b98f88a755be2db92602e633d7200c
Heney Goyani <bharatgoyani8788@gmail.com>
2025-11-28 19:59:37 +0530

    pr-8

---
+++ pr-8/middleware/sendEmail.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+
+
+exports.sendEmail = async (msg) => {
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        port: 587,
+        secure: false,
+        auth: {
+            user: "pruthvirajsolanki125@gmail.com",
+            pass: "eqwpcbtaezlrygxs",
+        },
+    })
+
+    const info = await transporter.sendMail(msg)
+    console.log("Message sent:", info.messageId);
+    return info;
+}
